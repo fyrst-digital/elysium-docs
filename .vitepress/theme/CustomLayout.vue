11271a947eec9a512780ac250ac9c1ebfcf5cd5c
jinroh <robsenq@gmail.com>
2023-06-18 16:35:08 +0200

    disable  lang redirect

--- .vitepress/theme/CustomLayout.vue
+++ .vitepress/theme/CustomLayout.vue
@@ -16,68 +16,7 @@ const isNotFound = computed(() => {
   return page.value.isNotFound || page.value.frontmatter['404'] ? true : false;
 })
 
-/*
-watchEffect(async () => {
 
-  if (page.value.isNotFound === true) {
-    let locales = site.value.locales
-    let browserLang = navigator.language.split("-")[0]
-
-    if (lang.value !== "en" && locales.hasOwnProperty(browserLang)) {
-      router.go('/' + browserLang + '/404')
-      console.log('-----------------')
-    } else {
-      router.go('/404')
-      console.log('++++++++++++++', localeIndex.value, browserLang, lang)
-    }
-    
-  }
-})
-*/
-
-// array of multiple sources
-watch([lang, () => page.value.isNotFound], ([newLang, pageNotFound]) => {
-   //console.log(lang, pageNotFound, page.value.frontmatter['404'])
-  // let locales = site.value.locales
-  // let browserLang = navigator.language.split("-")[0]
-  console.log('watch:', isNotFound.value, newLang, lang.value, pageNotFound, page.value.frontmatter['404'])
-  /*
-
-  if (pageNotFound === true) {
-    if (locales.hasOwnProperty(newLang)) {
-      console.log('gsdjgk++++++++')
-      router.go('/' + newLang + '/404')
-    } else {
-      console.log('gsdjgk++++++++')
-      router.go('/404')
-    }
-  }
-  */
-
-  /*
-  if (pageNotFound === true || page.value.frontmatter['404'] === true) {
-    if (newLang !== "en") {
-      router.go('/' + newLang + '/404')
-      console.log('meddl A+', newLang)
-    } else {
-      router.go('/404')
-      console.log('meddl B+', newLang)
-    }
-  } else {
-    let locales = site.value.locales
-    let browserLang = navigator.language.split("-")[0]
-    console.log('page exists:',newLang, locales, browserLang)
-
-    if (browserLang !== newLang && locales.hasOwnProperty(browserLang)) {
-      console.log('go A')
-      router.go('/' + browserLang + route.path)
-    } else if(browserLang === locales.root.lang && browserLang !== newLang) {
-      console.log('go B')
-      router.go(route.path.replace('/' + newLang, ''))
-    }
-  }
-  */
-})
 /*
 watch(lang, async () => {
   console.log('watch lang', lang.value, page.value.isNotFound)
@@ -87,6 +26,7 @@ watch(lang, async () => {
 onMounted(() => {
   let browserLang = navigator.language.split("-")[0]
 
+  /*
   console.log(router, page.value.isNotFound, browserLang, lang.value, localeIndex.value, locales)
 
 
